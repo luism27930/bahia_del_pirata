25b0b53915279c6bbc4bd9587a5d630d14d72102
Murillo133 <erickmurillo133@gmail.com>
2020-10-18 16:51:06 -0600

    Sending messages to rabbitMQ

--- app/Console/Commands/ReceiveLinks.php
+++ app/Console/Commands/ReceiveLinks.php
@@ -4,6 +4,9 @@ namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
 
+use PhpAmqpLib\Connection\AMQPStreamConnection;
+use PhpAmqpLib\Message\AMQPMessage;
+
 class ReceiveLinks extends Command
 {
     /**
@@ -37,6 +40,22 @@ class ReceiveLinks extends Command
      */
     public function handle()
     {
-        return 0;
+
+        $connection = new AMQPStreamConnection('shrimp-01.rmq.cloudamqp.com', 5672, 'gafnmalf', 'dfidH6NSrF-w5gZkZ25zXNsVsViFLI7P');
+        $channel = $connection->channel();
+
+        $channel->queue_declare('hello', false, false, false, false);
+
+        echo " [*] Waiting for messages. To exit press CTRL+C\n";
+        $callback = function ($msg) {
+            echo ' [x] Received ', $msg->body, "\n";
+        };
+
+        $channel->basic_consume('hello', '', false, true, false, false, $callback);
+
+        while (count($channel->callbacks)) {
+            $channel->wait();
+        }
+
     }
 }
