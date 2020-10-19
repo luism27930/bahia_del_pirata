927f1ffa13189b87df3b3c7b7d581711b23346f5
Murillo133 <erickmurillo133@gmail.com>
2020-10-19 00:27:50 -0600

    Download videos md5sum

--- app/Consumer/Download.php
+++ app/Consumer/Download.php
@@ -11,31 +11,36 @@
 // 5  - flv        [240x400]
 // 17 - mp4        [144x176]
 
-$format = '22';
-$url = 'https://www.youtube.com/watch?v=bQL2FsHe7G4';
-$template = 'Downloads/%(id)s.%(ext)s';
-$string = ('youtube-dl '. escapeshellarg($url).' -f 18 -o '.
-    escapeshellarg($template));
+ $link_id = '24';
+// $url = 'https://www.youtube.com/watch?v=bQL2FsHe7G4';
+//  $template = 'Downloads/'. $link_id.'.%(ext)s';
+// $string = ('youtube-dl '. escapeshellarg($url).' -f 18 -o '.
+//     escapeshellarg($template));
     
-$descriptorspec = array(
-    0 => array("pipe", "r"), // stdin
-    1 => array("pipe", "w"), // stdout
-    2 => array("pipe", "w"), // stderr
-);
-$process = proc_open($string, $descriptorspec, $pipes);
-$stdout = stream_get_contents($pipes[1]);
-fclose($pipes[1]);
-$stderr = stream_get_contents($pipes[2]);
-fclose($pipes[2]);
-$ret = proc_close($process);
-$data = json_encode(array('status' => $ret, 'errors' => $stderr,
-    'url_orginal' => $url, 'output' => $stdout,
-    'command' => $string));
-echo $data;
+// $descriptorspec = array(
+//     0 => array("pipe", "r"), // stdin
+//     1 => array("pipe", "w"), // stdout
+//     2 => array("pipe", "w"), // stderr
+// );
+// $process = proc_open($string, $descriptorspec, $pipes);
+// $stdout = stream_get_contents($pipes[1]);
+// fclose($pipes[1]);
+// $stderr = stream_get_contents($pipes[2]);
+// fclose($pipes[2]);
+// $ret = proc_close($process);
+// $data = json_encode(array('status' => $ret, 'errors' => $stderr,
+//     'url_orginal' => $url, 'output' => $stdout,
+//     'command' => $string));
+// echo $data;
 
-echo ('\n ESTO '.$pipes[2]);
+// echo ('\n RUTA '.$template.'mp4');
+$path = 'Downloads/'.$link_id.'.mp4';
+$md5sum = md5_file($path);
 
 
+// 10368434e472a96a0921dc7e0a4c6b9
+
+// a582f4dfde5389eb0a60f6d0f5048c13
 
 // {"status":0,"errors":"",
 //     "url_orginal":"https:\/\/www.youtube.com\/watch?v=bQL2FsHe7G4",
